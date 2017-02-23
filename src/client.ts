96c9852afbf8223d46a796ab9472ab76ea9267df
Grant Evans <grant.evans300@gmail.com>
2017-02-22 22:25:53 -0800

    tried to work on lab

--- src/client.ts
+++ src/client.ts
@@ -9,11 +9,13 @@ const net = require('net'),
     
 
 client.on('data', function(data) { //when we get data
-    
+   console.log("Received: "+data); //output it
 });
 
 client.on('close', function() { //when connection closed
-    
+   console.log('Connection closed');
+
+   process.exit(0);
 });
 
 
@@ -22,6 +24,35 @@ var PORT = 3000;
 
 //connect to the server
 client.connect(PORT, HOST, function() {
+   console.log('Connected to: ' + HOST + ':' + PORT);
+
+   //send message to server
+   client.write("Hello server, I'm the client!");
+
+       // prompt the user
+    io.setPrompt('> ');
+    io.prompt();
+
+    io.on('line', function(line){
+        switch(line.trim()) {
+        case 'exit':
+            client.end();
+            console.log('client disconnected');
+            process.exit(0);
+            break;
+        default:
+            client.write(line);
+            break;
+        }
+        // then prompt again
+        io.prompt();
+    }).on('close', function() {
+        //end the connection to the server if the user manually closes
+        client.end();
+        console.log('client disconnected');
+        process.exit(0);
+    });
+
 
 });
 
--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,19 @@ let server:net.Server = net.createServer();
 // when the server is connected
 server.on('connection', function(socket:net.Socket){
 
+    
+   //we've established a socket to use
+
+   //send a message to the socket
+   socket.write('Hello you!\n');
+
+   //close the connection
+   //socket.end();
+
     // when data is sent to the socket
     socket.on('data', function(data){
-        //
+        socket.write('(type something and hit return)\n');
+        return;
     });
 
     socket.on('close', function(){
